4238c298080385fe794d3fa69cfb227282c9bd9b
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-04-28 04:43:42 +0530

    Fetch data from API with redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,14 +1,24 @@
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
-
+import { useLazyGetSummaryQuery } from "../services/article";
 const Demo = () => {
 	const [article, setArticle] = useState({
 		url: "",
 		summary: "",
 	});
 
+	const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
 	const handleSubmit = async (e) => {
-		alert("submited");
+		e.preventDefault();
+		const { data } = await getSummary({ articleUrl: article.url });
+
+		if (data?.summary) {
+			const newArticle = { ...article, summary: data.summary };
+
+			setArticle(newArticle);
+			console.log(newArticle);
+		}
 	};
 	return (
 		<section className="mt-16 w-full max-w-xl">
@@ -16,7 +26,7 @@ const Demo = () => {
 			<div className="flex flex-col w-full gap-2">
 				<form
 					className="relative flex justify-center items-center"
-					onSubmit={() => handleSubmit()}
+					onSubmit={handleSubmit}
 				>
 					<img src={linkIcon} className="absolute left-0 my-2 ml-3 w-5" />
 					<input
--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,31 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// .env -> VITE_RAPID_API_ARTICLE_KEY=
+// const rapidAPiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY
+
 export const articleApi = createApi({
 	reducerPath: "articleApi",
+	baseQuery: fetchBaseQuery({
+		baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+		prepareHeaders: (headers) => {
+			headers.set(
+				"X-RapidAPI-Key",
+				"611f842b45msh58bee9c75b808b2p184a90jsn6103a871eaa9"
+			);
+			headers.set(
+				"X-RapidAPI-Host",
+				"article-extractor-and-summarizer.p.rapidapi.com"
+			);
+
+			return headers;
+		},
+	}),
 	endpoints: (builder) => ({
 		getSummary: builder.query({
-			query: (prams) => "test",
+			query: (params) =>
+				`/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
 		}),
 	}),
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;
